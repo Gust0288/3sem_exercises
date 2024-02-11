eabd837aa982295a7f6ae846a372d7e31e366d0a
Gustav <gubo0001@stud.kea.dk>
2024-02-11 21:16:27 +0100

    more

--- profanity_filter/js.js
+++ profanity_filter/js.js
@@ -6,27 +6,28 @@ const curseWords = [
 const textElement = document.querySelector("p");
 const replaceButton = document.getElementById("replaceButton");
 
+
 replaceButton.addEventListener("click", () => {
     const originalText = textElement.innerText;
     let newText = originalText;
-    let foundCurseWord = false;
-  
+    let isSFW = false;
+   
+    
     curseWords.forEach((word) => {
-        if (originalText.includes(word.bad)) {
-            newText = newText.replaceAll(word.bad, word.good);
-        } else{
-            foundCurseWord = true; // Set foundCurseWord to true when replacing the bad word
-        }      
+      if (originalText.includes(word.bad)) {
+        newText = newText.replaceAll(word.bad, word.good);
+      } else{
+        isSFW = true; // Set foundCurseWord to true when replacing the bad word
+      }      
     });
-    if (foundCurseWord) {
+    if (isSFW) {
       alert("It already is SFW");
       console.log("show up alert!!")
     } else {
       textElement.innerText = newText;
+      textElement.style.backgroundColor = "green";
     }
   
   });
-
-// alert dosn't work
 
 
